5f542146442c83f95cb45bcd3e74cfaf731a3ec5
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-03-27 17:02:20 -0300

    criado primeiro banco de dados

---
+++ banco_de_dados1.sql
@@ -0,0 +1,12 @@
+use cadastro;
+create database cadastro;
+create table pessoas(
+nome varchar(30),
+idade tinyint,
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+describe pessoas;
