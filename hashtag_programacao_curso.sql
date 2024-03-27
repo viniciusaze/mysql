ddaed828a8e2e26d3b19d5c8479e505adf868ea8
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-03-27 20:47:36 -0300

    curso de sql hashtag programação

---
+++ hashtag_programacao_curso.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE hashtag;
+USE hashtag;
+CREATE TABLE Vendas(
+	ID_venda INT,
+    Curso VARCHAR(100),
+    Aluno VARCHAR(100),
+    Estado VARCHAR(100),
+    Valor DECIMAL(10, 2)
+    );
+
+## Continua no curso de SQL da hastag programação
