a79fe9fb7a94a146b817696a9748502420633d27
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-03-28 16:20:21 -0300

    update bd1

--- banco_de_dados1.sql
+++ banco_de_dados1.sql
@@ -1,12 +1,20 @@
-use cadastro;
-create database cadastro;
-create table pessoas(
-nome varchar(30),
-idade tinyint,
-sexo char(1),
-peso float,
-altura float,
-nacionalidade varchar(20)
-);
+# CRIANDO O BANCO DE DADOS --------------------
 
-describe pessoas;
+CREATE DATABASE cadastro
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+# CRIANDO A TABELA PESSOAS --------------------
+
+CREATE TABLE pessoas(
+	id INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(30) NOT NULL,
+    nascimento DATE,
+    sexo ENUM('M','F'),
+    peso DECIMAL(5,2),
+    altura DECIMAL(3,2),
+    nacionalidade VARCHAR(20) DEFAULT 'Brasil',
+    PRIMARY KEY (id)
+)DEFAULT CHARSET = utf8mb4;
+
+DESCRIBE pessoas;
