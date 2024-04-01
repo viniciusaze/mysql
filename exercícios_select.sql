b80c0853b55051ed4a89b5ea284b6d0e78e264f9
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-04-01 15:46:32 -0300

    resolvidos exercícios sql

---
+++ exercícios_select.sql
@@ -0,0 +1,37 @@
+# EXERCÍCIOS --------------------------------------
+select * from gafanhotos;
+# 1 
+select nome from gafanhotos
+where sexo = 'F';
+
+# 2
+select * from gafanhotos
+where nascimento between '2000/01/01' and '2015/12/31';
+
+# 3
+select nome from gafanhotos
+where sexo = 'M' and profissao = 'Programador';
+
+# 4
+select * from gafanhotos
+where sexo = 'F' and nacionalidade = 'Brasil' and nome like 'J%';
+
+# 5
+select nome, nacionalidade from gafanhotos
+where nome like '%Silva%' and nacionalidade != 'Brasil' and peso < '100';
+
+# 6
+select max(altura) from gafanhotos
+where sexo = 'M' and nacionalidade = 'Brasil';
+
+# 7
+select avg(peso) from gafanhotos;
+
+# 8
+select min(peso) from gafanhotos
+where sexo = 'F' and nacionalidade != 'Brasil' and 
+nascimento between '1990/01/01' and '2000/12/31';
+
+# 9 
+select count(altura) from gafanhotos
+where sexo = 'F' and altura > '1.90';
